e60d88d26483b32e997da6ee26a5ea440ed58025
realnikk <pesochek2019@gmail.com>
2023-09-27 12:11:37 +0300

    fix bugs

--- lab4_sitairis/src/XMLParser.java
+++ lab4_sitairis/src/XMLParser.java
@@ -6,7 +6,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Variant1 {
+public class XMLParser {
     public static List<Application> appList = new ArrayList<>();
     public static void main(String[] args) {
         try {
@@ -58,93 +58,5 @@ public class Variant1 {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-}
-
-class Application {
-    private String id;
-    private String name;
-    private String company;
-    private String releaseDate;
-    private String lastUpdate;
-    private String ageLimit;
-    private Double price;
-    private Integer reviews;
-    private Double rating;
-    private String description;
-
-    public String getID() {
-        return id;
-    }
-    public void setID(String id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getCompany() {
-        return company;
-    }
-    public void setCompany(String company) {
-        this.company = company;
-    }
-    public String getReleaseDate() {
-        return releaseDate;
-    }
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-    public String getLastUpdate() {
-        return lastUpdate;
-    }
-    public void setLastUpdate(String lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-    public String getAgeLimit() {
-        return ageLimit;
-    }
-    public void setAgeLimit(String ageLimit) {
-        this.ageLimit = ageLimit;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-    public Integer getReviews() {
-        return reviews;
-    }
-    public void setReviews(Integer reviews) {
-        this.reviews = reviews;
-    }
-    public Double getRating() {
-        return rating;
-    }
-    public void setRating(Double rating) {
-        this.rating = rating;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getInfo() {
-        return "Application Info...\n" +
-                "App ID " + id + '\n' +
-                "App name: " + name + '\n' +
-                "Company-developer: " + company + '\n' +
-                "Release date: " + releaseDate + '\n' +
-                "Last update: " + lastUpdate + '\n' +
-                "Age limit: " + ageLimit + '\n' +
-                "Price of premium version: " + price + '\n' +
-                "Num of reviews: " + reviews + '\n' +
-                "Rating on a 5-point scale: " + rating + '\n' +
-                "Description:" + '\n' + description + '\n' +
-                "__________________________________________________________";
     }
 }
